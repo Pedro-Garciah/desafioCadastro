8c9bd78a9272f0804eccc5c7c515e6986065f701
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2025-04-01 00:11:59 -0300

    feat: adiciona leitura e exibicao de formulario

--- src/service/PetService.java
+++ src/service/PetService.java
@@ -3,11 +3,10 @@ package service;
 import repository.PetRepository;
 
 public class PetService {
+
     public static void savePet() {
         for (String question : PetRepository.showForm()) {
             System.out.println(question);
         }
-
-
     }
 }
