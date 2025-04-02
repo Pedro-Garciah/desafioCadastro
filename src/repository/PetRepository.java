a3b8d31d515a910398b02c9a18a9ccb620d414c6
Pedro Garcia <pedro.hage78@gmail.com>
2025-04-02 20:31:42 -0300

    feat: cadastra novos Pets

--- src/repository/PetRepository.java
+++ src/repository/PetRepository.java
@@ -1,9 +1,14 @@
 package repository;
 
+import domain.Pet;
+
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class PetRepository {
@@ -20,5 +25,32 @@ public class PetRepository {
         }
 
         return questions;
+    }
+
+    public static void savePet(Pet pet, List<String> answers) {
+
+        LocalDateTime ldt = LocalDateTime.now();
+        String formattedDateTime = ldt.format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmm"));
+        String formattedPetName = pet.getName().toUpperCase().trim().replaceAll(" +", "");
+
+        Path path = Paths.get("petsCadastrados/" + formattedDateTime + "-" + formattedPetName + ".txt");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+
+            if (Files.notExists(path))
+                Files.createFile(path);
+
+            int contador = 1;
+
+            for (String answer : answers) {
+                writer.write(contador + " - " + answer);
+                writer.newLine();
+                contador++;
+            }
+            writer.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/service/PetService.java
+++ src/service/PetService.java
@@ -1,10 +1,16 @@
 package service;
 
+import domain.Enums.Sex;
+import domain.Enums.Type;
+import domain.Pet;
 import repository.PetRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PetService {
+    static final String EMPTY_INFO = "NAO INFORMADO";
     static Scanner sc = new Scanner(System.in);
 
     public static void menu() {
@@ -39,8 +45,107 @@ public class PetService {
     }
 
     public static void savePet() {
+        List<String> answers = new ArrayList<>();
+
         for (String question : PetRepository.showForm()) {
             System.out.println(question);
+
+            if (question.startsWith("4 -")) {
+                answers.add(validateAddress());
+                continue;
+            }
+
+            answers.add(sc.nextLine().trim());
         }
+
+        Pet pet = new Pet();
+
+        pet.setName(validateName(answers.get(0)));
+        pet.setType(validatePetType(answers.get(1)));
+        pet.setSex(validatePetSex(answers.get(2)));
+        pet.setAddress(answers.get(3));
+        pet.setAge(validateAge(answers.get(4)));
+        pet.setWeight(validateWeight(answers.get(5)));
+        pet.setRace(validateRace(answers.get(6)));
+
+        PetRepository.savePet(pet, answers);
+    }
+
+    public static String validateAddress() {
+        System.out.println("Qual o número da casa?");
+        int number = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Qual a cidade?");
+        String city = sc.nextLine();
+
+        System.out.println("Qual a rua?");
+        String street = sc.nextLine();
+
+        return street + ", " + number + ", " + city;
+    }
+
+    public static String validateName(String text) {
+        if (text.isEmpty())
+            return EMPTY_INFO;
+
+        if (text.matches("([a-zA-Z]+ +)+[a-zA-Z]+"))
+            return text;
+        else throw new IllegalArgumentException("Digite um nome e sobrenome válidos.");
+    }
+
+    public static Type validatePetType(String text) {
+        if (text.trim().equalsIgnoreCase("gato"))
+            return Type.GATO;
+        if (text.trim().equalsIgnoreCase("cachorro"))
+            return Type.CACHORRO;
+        else
+            throw new IllegalArgumentException("Selecione um tipo válido (Cachorro ou Gato)");
+    }
+
+    public static Sex validatePetSex(String text) {
+        if (text.trim().equalsIgnoreCase("macho"))
+            return Sex.MALE;
+        if (text.trim().equalsIgnoreCase("femea")) {
+            return Sex.FEMALE;
+        } else
+            throw new IllegalArgumentException("Selecione um sexo válido (Macho ou Femea)");
+    }
+
+    public static String validateWeight(String text) {
+        if (text.isEmpty())
+            return EMPTY_INFO;
+
+        if (text.contains(","))
+            text = text.replace(",", ".");
+
+        double weight = Double.parseDouble(text);
+
+        if (weight > 60 || weight < 0.5)
+            throw new IllegalArgumentException("Digite um peso válido (entre 0,5kg e 60kg");
+
+        return String.valueOf(weight);
+    }
+
+    public static String validateAge(String text) {
+        if (text.isEmpty())
+            return EMPTY_INFO;
+
+        int age = Integer.parseInt(text);
+        if (age > 20)
+            throw new IllegalArgumentException("Digite uma idade válida(menor que 20)");
+
+//        if (age<1)
+//            age = 0.5 //
+        return String.valueOf(age);
+    }
+
+    public static String validateRace(String text) {
+        if (text.isEmpty())
+            return EMPTY_INFO;
+
+        if (text.matches("([a-zA-Z]+ +)*[a-zA-Z]+"))
+            return text;
+
+        throw new IllegalArgumentException("A raça não pode conter caracteres especiais.");
     }
 }
