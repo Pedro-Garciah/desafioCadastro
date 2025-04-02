a1e9b9225d62b9d4ff34ac087c606d33db9f29c7
Pedro Garcia <pedro.hage78@gmail.com>
2025-04-02 18:19:24 -0300

    feat: criado menu inicial

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,6 @@ import service.PetService;
 
 public class Main {
     public static void main(String[] args) {
-        PetService.savePet();
+        PetService.menu();
     }
 }
--- src/service/PetService.java
+++ src/service/PetService.java
@@ -2,7 +2,42 @@ package service;
 
 import repository.PetRepository;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class PetService {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void menu() {
+        boolean showMenu = true;
+
+        while (showMenu) {
+            System.out.print(
+                    """
+                            1. Cadastrar um novo pet
+                            2. Alterar os dados do pet cadastrado
+                            3. Deletar um pet cadastrado
+                            4. Listar todos os pets cadastrados
+                            5. Listar pets por algum critério (idade, nome, raça)
+                            6. Sair
+                            >>\s""");
+
+            // Lançar exceção customizada para inputs incorretos
+            int option = Integer.parseInt(sc.nextLine());
+
+            switch (option) {
+                case 1 -> savePet();
+                case 2 -> System.out.println("case 2");
+                case 3 -> System.out.println("case 3");
+                case 4 -> System.out.println("case 4");
+                case 5 -> System.out.println("case 5");
+                case 6 -> {
+                    System.out.println("Encerrando programa...");
+                    showMenu = false;
+                }
+            }
+        }
+    }
 
     public static void savePet() {
         for (String question : PetRepository.showForm()) {
